40a324a35e7f1f8d8a41537c8823cb7526490bf6
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-15 19:01:36 +0530

    creating object in heap using pointer

---
+++ object_in_heap.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class rectangle
+{
+	public:
+	int length;
+	int breadth;
+	int area()
+	{
+		return length*breadth;
+	}
+	int perimeter()
+	{
+		return 2*(length+breadth);
+	}
+};
+int main()
+{
+	rectangle *ptr=new rectangle;
+	ptr->length=10;
+	ptr->breadth=20;
+	cout<<ptr->area()<<"\n";
+	cout<<ptr->perimeter();
+	return 0;
+}
