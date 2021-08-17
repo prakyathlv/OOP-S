513d7e7cd6f9bf7f94bb9555b64c9f34d5a32d89
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-17 20:21:52 +0530

    sum of complex number using operator overloading

---
+++ operator_overloading.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+	private:
+		int real;
+		int img;
+	public:
+		complex(int r=0,int i=0)
+		{
+			real=r;
+			img=i;
+		}
+		complex operator+(complex c)
+		{
+			complex temp;
+			temp.real=real+c.real;
+			temp.img=img+c.img;
+			return temp;
+		}
+		void display()
+		{
+			cout<<real<<"+i"<<img<<endl;
+		}
+};
+int main()
+{
+	complex c1(3,7),c2(8,5),c3;
+	c3=c2+c1;
+	c3.display();
+	
+}
